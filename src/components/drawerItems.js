7323bf8eedf8eb024d1b23d399ba252aeed96441
Roopesh Patel <roops.pesh@gmail.com>
2020-05-25 10:47:34 +0100

    typo

--- src/components/drawerItems.js
+++ src/components/drawerItems.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Divider, List } from '@material-ui/core'
 import HomeIcon from '@material-ui/icons/Home'
 
-import DrawerItem from './DrawerItem'
+import DrawerItem from './drawerItem'
 
 const DrawerItems = ({ handleListItemClick, classes }) => {
   return (
